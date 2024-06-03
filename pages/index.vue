3738893ff98c57220661050123c575283e3da113
Aspeezee <ghimireshivaprashad@gmail.com>
2024-06-03 16:35:59 +0545

    nuxt

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="mx-0 py-0  text-9xl text-center font-bold bg-slate-300" >
+            <h1>index</h1>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style> 
